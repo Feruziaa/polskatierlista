0864842fedaa4b7446f67cdf30acf8eb531572bb
Feruziaa <kacper11.cz11@gmail.com>
2025-08-05 08:32:06 +0200

    Update SearchBar.js

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,1 +1,13 @@
-// placeholder
+// components/SearchBar.js
+export default function SearchBar({ search, setSearch }) {
+  return (
+    <div className="searchbar">
+      <input
+        type="text"
+        placeholder="Wpisz nick..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </div>
+  );
+}
