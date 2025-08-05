b3cee6443318d8c7e14c99dfc4f3448ecbc73bb3
Feruziaa <kacper11.cz11@gmail.com>
2025-08-05 08:31:42 +0200

    Update PlayerCard.js

--- components/PlayerCard.js
+++ components/PlayerCard.js
@@ -1,1 +1,21 @@
-// placeholder
+// components/PlayerCard.js
+export default function PlayerCard({ player }) {
+  return (
+    <div className="player-card">
+      <div className="player-info">
+        <img src={`/skins/${player.skin}`} alt={player.name} />
+        <div>
+          <div>{player.name}</div>
+          <div>Punkty: {player.points}</div>
+        </div>
+      </div>
+      <div className="tiers">
+        {player.tiers.map((tier, index) => (
+          <div key={index}>
+            <img src={`/icons/${tier.icon}`} alt={tier.name} className="tier-icon" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
